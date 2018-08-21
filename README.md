71ba732dd2d9a1a9011631bef3be040ebb0ab5da
Perry <pengyu_wang@163.com>
2018-08-21 11:19:15 +0800

    The first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+云课堂昵称：PerryWong
 # StudyGit
 study git
---
+++ Restaurant-Maven-Multi/Restaurant/src/main/java/com/netease/NoodlesServlet.java
@@ -0,0 +1,31 @@
+package com.netease;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.netease.Kitchen;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NoodlesServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	Logger log = LoggerFactory.getLogger(NoodlesServlet.class);
+    	 
+        log.info("------- Initializing ----------------------");
+    	
+    	PrintWriter writer = response.getWriter();
+
+        String vegetable = request.getParameter("vegetable");
+        log.info("------->>> vegetable = "+ vegetable);
+        String noodles = Kitchen.makeNoodles(vegetable);
+        writer.println(noodles);
+        log.info("------- Finished ----------------------");
+    }
+}
